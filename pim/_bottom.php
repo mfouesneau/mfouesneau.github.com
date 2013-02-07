8bb0cb5a05fd0aac48d85435ff42cb5b46598053
Morgan Fouesneau <morgan@fenrir>
2013-02-06 22:27:35 -0800

    update

--- pim/_bottom.php
+++
@@ -1,7 +0,0 @@
-</div>
-<div style="clear: both; margin-top: 20px; padding-bottom: 30px;">
-<hr>
-<p style="text-align: right; padding-right: 10px;">Morgan Fouesneau - <span style="font-style: italic;"><?php echo date('F Y'); ?></span></p>
-</div>
-</body>
-</html>
